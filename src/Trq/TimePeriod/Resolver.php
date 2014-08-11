2d445477f96553ff3eeb3a40c9abcc19176b5b1a
Tony R Quilkey <trq@thorpesystems.com>
2014-08-11 10:34:54 +1000

    feature: slight imporovement to minute logic

--- src/Trq/TimePeriod/Resolver.php
+++ src/Trq/TimePeriod/Resolver.php
@@ -4,12 +4,15 @@ namespace Trq\TimePeriod;
 
 class Resolver
 {
+    protected $minutes;
+
     public function parse($period)
     {
+        $this->minutes = (int) substr($period, 0, -1);
     }
 
     public function getMinutes()
     {
-        return 10;
+        return $this->minutes;
     }
 }
